4933ee6a76e3e2cf585357186be6069f4060747f
and2long <and2long@gmail.com>
2016-08-18 15:26:30 +0800

    add progressbar

--- app/src/main/java/com/and2long/webviewdemo/MainActivity.java
+++ app/src/main/java/com/and2long/webviewdemo/MainActivity.java
@@ -1,55 +1,64 @@
 package com.and2long.webviewdemo;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebview;
     private String mUrl = "http://hostmagazine.jp/job/detail/?job_id=2656e39daa3cd77";
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         mWebview = (WebView) findViewById(R.id.webview);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressbar);
 
         WebSettings settings = mWebview.getSettings();
         settings.setJavaScriptEnabled(true);
-        settings.setBuiltInZoomControls(true);
+        //settings.setBuiltInZoomControls(true);
         mWebview.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if (url.startsWith("tel:")){
+                if (url.startsWith("tel:")) {
                     Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
                     startActivity(intent);
-                } else if(url.startsWith("http:") || url.startsWith("https:")) {
+                } else if (url.startsWith("http:") || url.startsWith("https:")) {
                     view.loadUrl(url);
                 }
                 return true;
             }
         });
-
-
-        findViewById(R.id.load).setOnClickListener(new View.OnClickListener() {
+        mWebview.setWebChromeClient(new WebChromeClient() {
             @Override
-            public void onClick(View v) {
-                load();
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    if (View.INVISIBLE == mProgressBar.getVisibility()) {
+                        mProgressBar.setVisibility(View.VISIBLE);
+                    }
+                    mProgressBar.setProgress(newProgress);
+                }
             }
         });
-    }
 
-    private void load() {
         mWebview.loadUrl(mUrl);
     }
+
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -57,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
             mWebview.goBack();
             return true;
         }
-
         finish();
         return false;
     }
